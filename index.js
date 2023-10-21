14d417e6e9f1e69a67868c9f6c83d278bbe3a813
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2023-10-21 20:10:43 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import Home2 from './views/Home2/Home';
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+<Home2 />
+);
+
